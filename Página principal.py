c7c102a20ad05d6869722cf2e1d07cb14d3b1fe9
datosargentinosjuniors <datosargentinosjuniors@gmail.com>
2026-02-08 22:24:51 -0300

    Update Página principal.py

--- Página principal.py
+++ Página principal.py
@@ -43,7 +43,6 @@ st.markdown("""
         <p class='custom-text'>
             📌 <em>Detalles a tener en cuenta:</em><br><br>
             La aplicación se nutre de los datos de Skill Corner automáticamente desde una conexión a la API de la empresa, por lo que no hace falta una carga manual. La información puede demorar hasta 24 horas tras el final del partido para aparecer acá (dependemos exclusivamente de la empresa que disponibiliza los datos).
-            De todas maneras, cada página tiene un botón de actualización por si hace falta realizar la consulta a la API nuevamente.
     </div>
 """, unsafe_allow_html=True)
 
